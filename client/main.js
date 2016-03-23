2378a87504b381c94d04fbb61af357cb79bdd03b
Mark Shust <mark@shust.com>
2016-03-23 13:48:30 -0400

    Added keen-js sample

--- client/main.js
+++ client/main.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import {mount} from 'react-mounter';
+import Keen from 'keen-js';
 
-const Welcome = ({name}) => <div>Hello, {name}</div>;
+const Welcome = React.createClass({
+  componentDidMount() {
+    const client = new Keen({
+      projectId: 'KEEN_PROJECT_ID_GOES_HERE',
+      readKey: 'KEEN_READ_KEY_GOES_HERE'
+    });
+
+    Keen.ready(() => {
+      const query = new Keen.Query('count', {
+        eventCollection: 'userRegistered',
+        timeframe: 'this_14_days',
+        timezone: 'UTC'
+      });
+
+      client.draw(query, document.getElementById('my-chart'), {});
+    });
+
+  },
+
+  render() {
+    <div>
+      <div>Hello, {this.props.name}</div>
+      <div id='my-chart'></div>
+    </div>
+  }
+});
 
 mount(Welcome, {name: 'Foo Bar'});
